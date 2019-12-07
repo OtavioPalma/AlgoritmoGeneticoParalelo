968b1947cd36e4ff37a83d23411376073e1d38e7
Otávio Messias Palma <otaviopalma@gmail.com>
2019-12-06 23:33:35 -0300

    Update: pequena correcao AG

--- AlgoritmoGeneticoParalelo/src/Main.java
+++ AlgoritmoGeneticoParalelo/src/Main.java
@@ -8,20 +8,19 @@ public class Main {
 
     public static void main(String[] args)
             throws RemoteException, MalformedURLException, InterruptedException, NotBoundException {
-        AlgoritmoGenetico ag = new AlgoritmoGenetico(1000, 10);
+        AlgoritmoGenetico ag = new AlgoritmoGenetico(5000, 10);
         RMIServer server = new RMIServer();
 
         new Scanner(System.in).nextLine();
 
         server.setObjectReference("172.16.104.117");
 
-        for (int i = 0; i < 10; i++) 
-        { 
-            ag.evolucao(10);
+        for (int i = 0; i < 10; i++) {
+            ag.evolucao(25);
             server.setLista(ag.getPopulacao());
             server.setNumIteracao(i);
             server.start();
-            ag.addLista(server.getNovaLista()); 
+            ag.addLista(server.getNovaLista());
         }
         System.out.println("Finalizado!");
         ag.mostrarPopulacao();
